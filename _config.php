097ce393cd2193acb8e4d2e173448e3fe43af299
Steven Orr <steve.a.orr@gmail.com>
2013-07-01 11:18:58 -0700

    Reordered definitions, bringing items that SHOULD be edited to the top and pushing those that SHOULD NOT to the bottom. Added more explanatory comments.

--- _config.php
+++ _config.php
@@ -13,11 +13,30 @@
  * @link      http://daf.csulb.edu/
  */
 
+ //
+// These configuration settings SHOULD be edited.
+//
+
 /**
- * Environment variable, used in settings configurations and in application.
+ * This controls whether the ASM is emailed when a user signs up for a workshop.
+ */
+$config_alertASM = false;
+
+/**
+ * This populates the administrative form's location select field, when creating a workshop.
+ */
+$config_locations = array(
+    'BLDG-RM' => 'Training Room Name, BLDG-RM',
+);
+
+//
+// The configuration settings below SHOULD NOT require editing.
+//
+
+/**
+ * Environment variable, used in settings configurations and in application. NO NEED TO EDIT!
  *
  * This must be set to one of the following:
- *
  *     development
  *     testing
  *     production
@@ -29,34 +48,26 @@
 define('ENVIRONMENT', apache_getenv("HTTPD_ENV"));
 
 /**
- * Constants, which CAN change per application environment.
+ * Constants, which CAN change per application environment. NO NEED TO EDIT!
+ *
+ * The order in which these are created IS important.
  */
-$cfg_app = parse_ini_file("_app.ini.php", true);
-define('DIR', '/'.$cfg_app['common']['host']);
-define('PATH_PARENT', $cfg_app['common']['localdocroot']."/".$cfg_app['common']['parentdir']);
-define('PATH_APP', PATH_PARENT.DIR);
-define('PATH_COMMON', PATH_PARENT.'/common');
-define('PATH_GROUP', $cfg_app['common']['externalwebroot']);
-define('NAME_GROUP', $cfg_app['common']['appname']);
-
-define('APP_HOST', 'http://'.$cfg_app[ENVIRONMENT]['apphost']);
-define('URL_PARENT', APP_HOST."/".$cfg_app['common']['parentdir']);
-define('URL_APP', URL_PARENT.DIR);
-define('URL_COMMON', URL_PARENT."/".$cfg_app['common']['assetsdir']);
-define('URL_GROUP', 'http://'.$cfg_app[ENVIRONMENT]['apphost'].PATH_GROUP);
+$config_app = parse_ini_file("_app.ini.php", true);
+define('DIR',            '/'.$config_app['common']['host']);
+define('PATH_PARENT',    $config_app['common']['localdocroot']."/".$config_app['common']['parentdir']);
+define('PATH_APP',       PATH_PARENT.DIR);
+define('PATH_COMMON',    PATH_PARENT.'/common');
+define('PATH_GROUP',     $config_app['common']['externalwebroot']);
+define('NAME_GROUP',     $config_app['common']['appname']);
+define('APP_HOST',       'http://'.$config_app[ENVIRONMENT]['apphost']);
+define('URL_PARENT',     APP_HOST."/".$config_app['common']['parentdir']);
+define('URL_APP',        URL_PARENT.DIR);
+define('URL_COMMON',     URL_PARENT."/".$config_app['common']['assetsdir']);
+define('URL_GROUP',      'http://'.$config_app[ENVIRONMENT]['apphost'].PATH_GROUP);
 define('BACKLINK_ADMIN', "<div id='back'><a href='".URL_APP."/admin/'>Return to Control Panel</a></div>");
-define('BACKLINK_APP', "<div id='back'><a href='".URL_APP."'>Return to ".NAME_GROUP."</a></div>");
-define('MAIL_GROUP', $cfg_app['common']['mailaddress']);
-
-define('CHUNK1', PATH_COMMON.'/'.$cfg_app['common']['guifile'].'1.php');
-define('CHUNK2', PATH_COMMON.'/'.$cfg_app['common']['guifile'].'2.php');
-define('CHUNK3', PATH_COMMON.'/'.$cfg_app['common']['guifile'].'3.php');
-define('CHUNK4', PATH_COMMON.'/'.$cfg_app['common']['guifile'].'4.php');
-
-/**
- * Variables.
- */
-$config_alertASM = false;
-$config_locations = array(
-    'ITS Training, BH-180D' => 'ITS Training Room, BH-180D',
-);
+define('BACKLINK_APP',   "<div id='back'><a href='".URL_APP."'>Return to ".NAME_GROUP."</a></div>");
+define('MAIL_GROUP',     $config_app['common']['mailaddress']);
+define('CHUNK1',         PATH_COMMON.'/'.$config_app['common']['guifile'].'1.php');
+define('CHUNK2',         PATH_COMMON.'/'.$config_app['common']['guifile'].'2.php');
+define('CHUNK3',         PATH_COMMON.'/'.$config_app['common']['guifile'].'3.php');
+define('CHUNK4',         PATH_COMMON.'/'.$config_app['common']['guifile'].'4.php');
