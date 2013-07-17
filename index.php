38e3dbd33c0eed092693dc5431145c9835207fd3
Steven Orr <steve.a.orr@gmail.com>
2013-07-17 16:37:46 -0700

    Fixed configuration string that was not coming across as a boolean value. Added a flag to the DEV index page to show if boolean working or not.

--- index.php
+++ index.php
@@ -20,7 +20,8 @@ $html = '';
 if (ENVIRONMENT !== 'development') {
     header("Location: ".URL_GROUP);
 } else {
-    $html  = "<html><head><title>temp page</title></head><body><p>Temporary ".NAME_GROUP." page.</p><ul>";
+    $flag  = ($config_alertASM)? 'ON' : 'OFF';
+    $html  = "<html><head><title>temp page</title></head><body><p>Temporary ".NAME_GROUP." page. Flag is {$flag}.</p><ul>";
     $html .= "<li><a href='list.php'>List</a></li><li><a href='calendar.php'>Calendar</a></li>";
     $html .= "<li><a href='review.php'>Reservation</a></li></ul></body></html>";
 }
