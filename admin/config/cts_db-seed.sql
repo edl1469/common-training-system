4897e05b67cba573d6f6c82942c7a30a93916976
Steve Orr <sorr@csulb.edu>
2014-09-03 09:29:04 -0700

    Start tracking database schema.

---
+++ admin/config/cts_db-seed.sql
@@ -0,0 +1,7 @@
+--
+-- Databases: `cts_training_dev`, `cts_training_test`, `cts_training`
+--
+
+-- --------------------------------------------------------
+
+INSERT INTO `location` VALUES ('1', 'TBD');
